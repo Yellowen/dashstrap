2201cc9bb8b487b9c67547d221b1c8602d4a4881
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2014-10-19 18:06:04 +0330

    javascript translation files improved

---
+++ app/assets/javascripts/dashstrap/locales/translations.fa.js
@@ -0,0 +1,5 @@
+angular.module('Dashboard').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('fa', {"Are sure you want to delete":"آیا از حذف اطمینان دارید","Click to pick a date":"برای انتخاب تاریخ کلیک کنید","Current Page":"صفحه جاری","Dashboard":"پنل کاربری","Facility Name":"نام وسیله","Go!":"برو!","Item(s)":"آیتم(ها)","Note":"متن","Number":"عدد","Online":"آنلاین","Price?":"قیمت؟","Search":"جستجو","Selected":"انتخاب شده","There is no data!":"اطلاعات یافت نشد","Toggle Select":"پین انتخاب","Total Pages":"کل صفحات","item(s)?":"آیتم(ها)؟"});
+/* jshint +W100 */
+}]);
