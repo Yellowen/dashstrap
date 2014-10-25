c3d6c93a43f95ee90a0311a5fb2e48158bd7c681
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2014-10-25 17:07:58 +0330

    some translation modified

---
+++ app/assets/javascripts/locales/translations.fa.js
@@ -0,0 +1,5 @@
+angular.module('Dashboard').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('fa', {"Are sure you want to delete":"آیا از حذف اطمینان دارید","Click to pick a date":"برای انتخاب تاریخ کلیک کنید","Current Page":"صفحه جاری","Dashboard":"پنل کاربری","Facility Name":"نام وسیله","Go!":"برو!","Item(s)":"آیتم(ها)","Note":"متن","Number":"عدد","Online":"آنلاین","Price?":"قیمت؟","Search":"جستجو","Selected":"انتخاب شده","There is no data!":"تاریخ نامعتبر","Toggle Select":"پین انتخاب","Total Pages":"کل صفحات","item(s)?":"آیتیم(ها)؟"});
+/* jshint +W100 */
+}]);
