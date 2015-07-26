2eb4cdf290523368345d7656b8b27d8a44463f91
Sameer Rahmani <lxsameer@gnu.org>
2015-07-26 15:46:42 +0430

    engine class backed to normal, no faalis extension any mode

--- lib/dashstrap/engine.rb
+++ lib/dashstrap/engine.rb
@@ -1,4 +1,3 @@
-require 'faalis/extension'
 require 'jquery-ui-rails'
 require 'font-awesome-rails'
 require 'select2-rails'
@@ -11,27 +10,12 @@ require 'rails-assets-jquery-sparkline'
 module Dashstrap
   class TemplateEngine < ::Rails::Engine
 
-    include Faalis::Extension::Base
-
     initializer 'dashboard.add_assets_for_precompile' do |app|
       Rails.application.config.assets.precompile += ['fontawesome-webfont.eot',
                                                      'fontawesome-webfont.woff',
                                                      'fontawesome-webfont.ttf',
                                                      'fontawesome-webfont.svg']
     end
-
-    def self.register_extension(name, klass)
-      Faalis::Extension.extensions[name] = klass
-    end
-
-    def self.override_generator_templates(template_path)
-      send(:define_singleton_method, 'generator_templates_path') do
-        template_path
-      end
-    end
-
-    register_extension 'dashstrap', self
-    override_generator_templates File.expand_path('../../generators/templates', __FILE__)
   end
 end
 
